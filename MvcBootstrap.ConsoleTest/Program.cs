9e4e8a9e398b9ae1af11eaa530de7229514df2e6
RockyMyx <myx8178633@163.com>
2013-12-27 16:22:43 +0800

    add ConsoleApp Test

---
+++ MvcBootstrap.ConsoleTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MvcBootstrap.DAO;
+using MvcBootstrap.EFModel;
+using System.Data.Objects;
+
+namespace MvcBootstrap.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DBEntity db = new DBEntity())
+            {
+                IEnumerable<Module> modules = db.GetModuleTree();
+                foreach (Module module in modules)
+                {
+                    Console.WriteLine(module.Name);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MvcBootstrap.ConsoleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcBootstrap.ConsoleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("MvcBootstrap.ConsoleTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8ef80e1a-e8aa-4615-9458-c80212dba48a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
