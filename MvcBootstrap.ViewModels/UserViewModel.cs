0930f41bcebb6fccf93ed1056e1b4acc79042d0f
RockyMyx <myx8178633@163.com>
2014-02-24 22:24:53 +0800

    add user view model

---
+++ MvcBootstrap.ViewModels/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcBootstrap.ViewModels
+{
+    public class UserViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string RealName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
